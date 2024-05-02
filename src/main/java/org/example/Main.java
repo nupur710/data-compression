ad6950a5dce7b5c2ec1a4a166bd0166bfb0be459
Nupur Singh <nupur.singh2410@gmail.com>
2024-05-02 20:53:03 +0530

    read from file

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        HuffmanCompression huffman= new HuffmanCompression();
+        var input= new StringBuilder();
+        try {
+            FileReader file = new FileReader("C:\\Users\\lenovo\\OneDrive\\Desktop\\Gutenberg.txt");
+            BufferedReader br = new BufferedReader(file);
+            int c;
+            while((c= br.read()) != -1) {
+                input.append((char) c);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(input.toString());
+        huffman.frequencyTable(input.toString().toCharArray());
+    }
+}
