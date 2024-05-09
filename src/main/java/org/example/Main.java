3fb81d4ee668f9978c03f52d54ad5756d0fa223e
Nupur Singh <nupur.singh2410@gmail.com>
2024-05-09 23:49:16 +0530

    rough

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,15 +9,29 @@ public class Main {
         var input= new StringBuilder();
         try {
             FileReader file = new FileReader(System.getProperty("user.dir")+"\\src\\main\\resources\\Gutenberg.txt");
+            FileWriter writer= new FileWriter(System.getProperty("user.dir")+"\\src\\main\\resources\\GutenbergEncoded.txt");
             BufferedReader br = new BufferedReader(file);
+            BufferedWriter bw= new BufferedWriter(writer);
             int c;
+            long start= System.currentTimeMillis();
             while((c= br.read()) != -1) {
                 input.append((char) c);
+//                System.out.print((char) c);
             }
-        } catch (IOException e) {
+            String text= huffman.compress(input.toString().toCharArray());
+            bw.write(text);
+            bw= new BufferedWriter(new FileWriter(System.getProperty("user.dir")+"\\src\\main\\resources\\GutenbergDecoded.txt"));
+            char[] decompressedText= huffman.decompress(text.toString().toCharArray());
+            bw.write(decompressedText);
+            long end= System.currentTimeMillis();
+            System.out.println("original: " + (end - start));
+            //huffman.frequencyTable(input.toString().toCharArray());
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
             e.printStackTrace();
+        } catch (IOException i) {
+            System.out.println("Error reading file: ");
+            i.printStackTrace();
         }
-        System.out.println(input.toString());
-        huffman.frequencyTable(input.toString().toCharArray());
     }
 }
