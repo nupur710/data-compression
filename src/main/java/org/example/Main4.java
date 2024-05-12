822f9b9e395255dda305dc9a44229203474bd837
Nupur Singh <nupur.singh2410@gmail.com>
2024-05-13 00:12:55 +0530

    read-write to encoded & decoded file

--- src/main/java/org/example/Main4.java
+++ src/main/java/org/example/Main4.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.BitSet;
 
 public class Main4 {
@@ -14,35 +15,13 @@ public class Main4 {
         while((c= br.read()) != -1) {
             text.append((char) c);
         }
-        String input= huffmanCompression.compress(text.toString().toCharArray());
-        char[] ch= input.toCharArray();
-        //char[] ch = {'1', '0', '1', '0', '0', '0', '1', '1', '1'};
-        BitSet bitSet= new BitSet(ch.length);
-        System.out.println(bitSet.length());
-        for(int i= 0; i<ch.length; i++) {
-            if(ch[i] == '1') {
-                bitSet.set(i);
-            }
-        }
-        writeBitSetToFile(bitSet, "write_bitset_to_file.txt");
 
-        char[] x= new char[bitSet.length()];
-        for(int i= 0; i < bitSet.length(); i++) {
-            if(bitSet.get(i)) {
-                x[i]= '1';
-            } else {
-                x[i]= '0';
-            }
-        }
-
+        String compressed= huffmanCompression.compress(text.toString().toCharArray());
+        char[] xy= huffmanCompression.decompress(compressed.toCharArray());
+        FileOutputStream outputStream= new FileOutputStream(System.getProperty("user.dir")+"\\src\\main\\resources\\Gutenberg.txt");
+        BufferedWriter writer= new BufferedWriter(new FileWriter(System.getProperty("user.dir")+"\\src\\main\\resources\\decoded_text.txt"));
+        writer.write(xy);
+        writer.close();
     }
 
-    public static void writeBitSetToFile(BitSet bitSet, String filename) throws IOException {
-        FileOutputStream fos = new FileOutputStream(filename);
-        ObjectOutputStream oos = new ObjectOutputStream(fos);
-        oos.writeObject(bitSet);
-        oos.close();
-        fos.close();
-        System.out.println("BitSet written to file: " + filename);
-    }
 }
